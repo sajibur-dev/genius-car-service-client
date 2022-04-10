a63c84d3ce440afd22ae9ef03f2c15a36fd82c8e
sajibur-rahman <sajiburr21@gmail.com>
2022-04-10 21:28:49 +0600

    basic routing is done

---
+++ src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './About.module.css';
+
+const About = () => {
+    return (
+        <div className={styles.about}>
+            <h1>this is about</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Services.module.css';
+
+const Services = () => {
+    return (
+        <div className={styles.services}>
+            <h2>this is services</h2>
+        </div>
+    );
+};
+
+export default Services;
