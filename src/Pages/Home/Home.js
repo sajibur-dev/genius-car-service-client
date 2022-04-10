46bc996fe39bc178365101d3b381f8ea5c9f47b2
sajibur-rahman <sajiburr21@gmail.com>
2022-04-10 22:10:56 +0600

    create fake services is done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './Home.module.css';
+import Services from './Services/Services';
 
 const Home = () => {
     return (
         <div className={styles.home}>
-            <h1>this is home</h1>
+            <Services/>
         </div>
     );
 };
