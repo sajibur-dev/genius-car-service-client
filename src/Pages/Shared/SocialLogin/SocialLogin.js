18c1ec2cd21dbfb7a5bae58cc5e9c6c0995a1d79
sajibur-rahman <sajiburr21@gmail.com>
2022-04-14 21:03:27 +0600

    social login is done

--- src/Pages/Shared/SocialLogin/SocialLogin.js
+++ src/Pages/Shared/SocialLogin/SocialLogin.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import facebookLogo from "../../../assets/images/facebook.png";
 import githubLogo from "../../../assets/images/github.png";
 import Googlelogo from "../../../assets/images/google.png";
+import auth from "../../../firebase";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  
+  if (user) {
+    navigate("/");
+  }
   return (
     <div>
       <div className="d-flex align-items-center">
@@ -11,15 +20,22 @@ const SocialLogin = () => {
         <p className="mt-2 mx-2">or</p>
         <div style={{ height: "1px" }} className="bg-primary w-50"></div>
       </div>
+      {error && <p className="text-danger">{error.message}</p>}
       <div>
-        <button className="btn btn-info d-block mx-auto w-50 my-3">
-          <img src={Googlelogo} alt="" /> <span className="px-3">google login</span>
+        <button
+          className="btn btn-info d-block mx-auto w-50 my-3"
+          onClick={() => signInWithGoogle()}
+        >
+          <img src={Googlelogo} alt="" />{" "}
+          <span className="px-3">google login</span>
         </button>
         <button className="btn btn-info d-block mx-auto w-50 my-3">
-          <img src={facebookLogo} alt="" /> <span className="px-3">facebook login</span>
+          <img src={facebookLogo} alt="" />{" "}
+          <span className="px-3">facebook login</span>
         </button>
         <button className="btn btn-info d-block mx-auto w-50 my-3">
-          <img src={githubLogo} alt="" /> <span className="px-3">github login</span>
+          <img src={githubLogo} alt="" />{" "}
+          <span className="px-3">github login</span>
         </button>
       </div>
     </div>
