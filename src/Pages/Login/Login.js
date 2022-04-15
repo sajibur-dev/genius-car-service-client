f2f051e8e157139caa3b711e9010ee8ed4baa694
sajibur-rahman <sajiburr21@gmail.com>
2022-04-15 11:30:37 +0600

    conditional css class set is done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,58 +1,90 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword
+} from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase";
 import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
-
 const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
-    const location = useLocation();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const from = location.state?.from?.pathname || '/' 
-    
-    const [
-      signInWithEmailAndPassword,
-      user,
-    ] = useSignInWithEmailAndPassword(auth)
-  
-  if(user){
-    navigate(from, {replace:true})
+  const from = location.state?.from?.pathname || "/";
+
+  const [signInWithEmailAndPassword, user, signInError] =
+    useSignInWithEmailAndPassword(auth);
+  const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
+
+  if (user) {
+    navigate(from, { replace: true });
   }
-  
-    const handleLogin = (e) => {
+
+  const handleLogin = (e) => {
     e.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    signInWithEmailAndPassword(email,password);
+    signInWithEmailAndPassword(email, password);
   };
-
-
-
 
   return (
     <div className="container w-50 mx-auto">
-        <h2 className="text-primary text-center mt-3">Login...</h2>
+      <h2 className="text-primary text-center mt-3">Login...</h2>
       <Form onSubmit={handleLogin}>
         <Form.Group className="mb-5" controlId="formBasicEmail">
-          <Form.Control type="email" className="p-3 border-2 border-dark fs-3 ouline-none" placeholder="Enter email" ref={emailRef} required/>
+          <Form.Control
+            type="email"
+            className="p-3 border-2 border-dark fs-3 ouline-none"
+            placeholder="Enter email"
+            ref={emailRef}
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-5" controlId="formBasicPassword">
-          <Form.Control className="p-3 border-2 border-dark fs-3 outline-none" type="password" placeholder="Password" ref={passwordRef} required/>
+          <Form.Control
+            className="p-3 border-2 border-dark fs-3 outline-none"
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+            required
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
+
+        <Button
+          className="w-50 d-block mx-auto"
+          variant="primary"
+          type="submit"
+        >
           login
         </Button>
       </Form>
-      <p>New to genius car ? <Link to='/signup' className="text-decoration-none fs-3">singup</Link></p>
-      <SocialLogin/>
+      {signInError && <p>{signInError?.message}</p>}
+      <p>
+        New to genius car ?{" "}
+        <Link to="/signup" className="text-primary text-decoration-none fs-3">
+          singup
+        </Link>
+      </p>
+      <p>
+        Forgate password ?{" "}
+        <Link
+          to="/signup"
+          onClick={async () => {
+            const email = emailRef.current.value;
+            await sendPasswordResetEmail(email);
+            alert("sent email");
+          }}
+          className="text-primary text-decoration-none fs-3"
+        >
+          reset password
+        </Link>
+      </p>
+      <SocialLogin />
     </div>
   );
 };
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,50 +1,78 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
-import auth from '../../firebase';
-import SocialLogin from '../Shared/SocialLogin/SocialLogin';
-
-
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 const Signup = () => {
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('');
-    const navigate = useNavigate();
-    const [
-        createUserWithEmailAndPassword,
-        user,
-    ] = useCreateUserWithEmailAndPassword(auth)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [agree, setAgree] = useState(false);
 
-    if(user){
-        navigate('/home')
-    }
+  const navigate = useNavigate();
+  const [createUserWithEmailAndPassword, user] =
+    useCreateUserWithEmailAndPassword(auth);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        createUserWithEmailAndPassword(email,password);
-    }
-    
+  if (user) {
+    navigate("/home");
+  }
 
-    return (
-        <div className="container w-50 mx-auto">
-        <h2 className="text-primary text-center mt-3">Signup...</h2>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <div className="container w-50 mx-auto">
+      <h2 className="text-primary text-center mt-3">Signup...</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-5" controlId="formBasicEmail">
-          <Form.Control type="email" className="p-3 border-2 border-dark fs-3 ouline-none" placeholder="Enter email" onBlur={(e) => setEmail(e.target.value)} required/>
+          <Form.Control
+            type="email"
+            className="p-3 border-2 border-dark fs-3 ouline-none"
+            placeholder="Enter email"
+            onBlur={(e) => setEmail(e.target.value)}
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-5" controlId="formBasicPassword">
-          <Form.Control className="p-3 border-2 border-dark fs-3 outline-none" type="password" placeholder="Password" onBlur={(e) => setPassword(e.target.value)} required/>
+          <Form.Control
+            className="p-3 border-2 border-dark fs-3 outline-none"
+            type="password"
+            placeholder="Password"
+            onBlur={(e) => setPassword(e.target.value)}
+            required
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+            onClick={() => setAgree(!agree)}
+            className={agree ? "text-primary" : "text-danger"}
+            type="checkbox"
+            label="terms and condition"
+          />
+        </Form.Group>
+
+        <Button
+          disabled={!agree}
+          className="w-50 mx-auto d-block"
+          variant="primary"
+          type="submit"
+        >
           signup
         </Button>
       </Form>
-      <p>already have an account ? <Link to='/login' className="text-decoration-none fs-3">login</Link></p>
-      <SocialLogin/>
+      <p>
+        already have an account ?{" "}
+        <Link to="/login" className="text-decoration-none fs-3">
+          login
+        </Link>
+      </p>
+      <SocialLogin />
     </div>
-    );
+  );
 };
 
 export default Signup;
